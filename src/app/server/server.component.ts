79b32755c316509f6e12a5cc5e9a4f9bdb2bd28c
ShikhaP12 <pshikha407@gmail.com>
2021-12-04 19:41:49 +0530

    2 files modified

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html'
-  //styleUrls: ['./server.component.css'],
 })
 
 export class ServerComponent {
@@ -12,5 +11,13 @@ export class ServerComponent {
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  allowNewServer = false;
+
+  constructor(){
+    setTimeout(()=> {
+      this.allowNewServer=true;
+    },2000);
   }
 }
