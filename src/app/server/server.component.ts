e4241ed0ad84a9c872015fd45780b41c1e6cbbc3
ShikhaP12 <pshikha407@gmail.com>
2021-12-05 13:00:25 +0530

    3 files modified

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
 })
-
 export class ServerComponent {
   serverID: number = 20;
   serverStatus: string = 'offline';
@@ -12,20 +11,23 @@ export class ServerComponent {
   allowNewServer = false;
   serverName1 = '';
   serverName2 = '';
+  serverCreated = false;
 
   getServerStatus() {
     return this.serverStatus;
   }
-  constructor(){
-    setTimeout(()=> {
-      this.allowNewServer=true;
-    },2000);
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
-  onCreateServer(){
-    this.serverCreationStatus = 'Server is created!!! Name is '+this.serverName1 ;
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus =
+      'Server is created!!! Name is ' + this.serverName1;
   }
 
-  onUpdateServerName1(event: Event){
-    this.serverName1=(<HTMLInputElement>event.target).value;
+  onUpdateServerName1(event: Event) {
+    this.serverName1 = (<HTMLInputElement>event.target).value;
   }
 }
